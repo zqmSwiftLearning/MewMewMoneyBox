8dc7a2e8e9395b221b73460e83352f5648194910
张青明 <zhangqingming@codinghacker.com>
2015-09-03 15:34:58 +0800

    个人信息vc

---
+++ MewMewMoneyBox/MewMewMoneyBox/Sections/UserInformation/Controller/UserInformationViewController.swift
@@ -0,0 +1,36 @@
+//
+//  UserInformationViewController.swift
+//  MewMewMoneyBox
+//
+//  Created by 张青明 on 15/9/2.
+//  Copyright (c) 2015年 极客栈. All rights reserved.
+//
+
+import UIKit
+
+/// 个人信息vc
+class UserInformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
