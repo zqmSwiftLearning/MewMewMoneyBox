8a7e501363aeccdefafcacfdbec5a5fdce7f8b69
张青明 <zhangqingming@codinghacker.com>
2015-09-03 15:40:37 +0800

    全局颜色

---
+++ MewMewMoneyBox/MewMewMoneyBox/Macro/ColorMacro.swift
@@ -0,0 +1,16 @@
+//
+//  ColorMacro.swift
+//  MewMewMoneyBox
+//
+//  Created by 张青明 on 15/8/31.
+//  Copyright (c) 2015年 极客栈. All rights reserved.
+//
+
+import UIKit
+
+//let GlobalColor = UIColor(red: 251.0/255.0, green: 80.0/255.0, blue: 38.0/255.0, alpha: 1.0)
+let GlobalColor = UIColor(red: 254.0/255.0, green: 103.0/255.0, blue: 50.0/255.0, alpha: 1.0)
+
+//class ColorMacro: NSObject {
+//   
+//}
