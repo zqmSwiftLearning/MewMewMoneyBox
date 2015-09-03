34858c4f1e4a123dbcccf589848f6ba07fa411f2
张青明 <zhangqingming@codinghacker.com>
2015-09-03 15:41:10 +0800

    通用的常量

---
+++ MewMewMoneyBox/MewMewMoneyBox/Macro/CommonConstants.swift
@@ -0,0 +1,29 @@
+//
+//  CommonConstants.swift
+//  MewMewMoneyBox
+//
+//  Created by 张青明 on 15/9/2.
+//  Copyright (c) 2015年 极客栈. All rights reserved.
+//
+//  通用的常量
+
+import UIKit
+
+// MARK: - 尺寸相关
+
+let kMainScreen               = UIScreen.mainScreen()
+let kMainScreenScale          = UIScreen.mainScreen().scale
+
+let kMainScreenBounds         = UIScreen.mainScreen().bounds
+let kMainScreenWidth :CGFloat = UIScreen.mainScreen().bounds.size.width
+let kMainScreenHeight:CGFloat = UIScreen.mainScreen().bounds.size.height
+
+
+// MARK: - 其它
+
+/// 系统偏好
+let kUserDefaults       = NSUserDefaults.standardUserDefaults()
+
+/// 通知中心
+let kNotificationCenter = NSNotificationCenter.defaultCenter()
+
