7fe8b3f5c252b39d86cefae1b69e61c76b07b6ea
张青明 <zhangqingming@codinghacker.com>
2015-09-03 15:39:31 +0800

    用户信息列表的vc和storyboard

---
+++ MewMewMoneyBox/MewMewMoneyBox/Sections/UserInfoList/View/UserInfoListHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  UserInfoListHeaderView.swift
+//  MewMewMoneyBox
+//
+//  Created by 张青明 on 15/8/31.
+//  Copyright (c) 2015年 极客栈. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoListHeaderView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
